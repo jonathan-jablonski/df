7e503b37587cab3244df9ede93b8ce6ddf5a6485
jonathan-jablonski <jonathanjablonski94@gmail.com>
2022-02-11 14:55:36 -0600

    Editing background video, going public and deploying onto GH Pages for working demo

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,33 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: false,
+    lightTextDesc: true,
+    topLine: 'About',
+    headline: 'Where We Come From',
+    description: 'Jonathan (JJ, guitar) and Jackson (JB, drums) hail from Dallas, TX, coming to your ears from the reach of the Internet.',
+    buttonLabel: 'Follow Us',
+    imgStart: true,
+    img: require('../../images/ww.png'),
+    alt: 'Dual Funeral',
+    dark: true,
+    primary: true,
+    darkText: false
+};
+
+export const homeObjTwo = {
+    id: 'credits',
+    lightBg: false,
+    lightText: false,
+    lightTextDesc: true,
+    topLine: 'Credits',
+    headline: 'Who Has Helped Us',
+    description: 'We greatly appreciate all of the time, support, and criticism from the artists that we have the privilege to work with.',
+    buttonLabel: 'HyperFollow Page',
+    imgStart: false,
+    img: require('../../images/df.png'),
+    alt: 'Dual Funeral',
+    dark: true,
+    primary: true,
+    darkText: false
+};
